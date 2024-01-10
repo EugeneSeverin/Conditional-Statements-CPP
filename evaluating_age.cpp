b97dad6824fc6a086c31b8ae93f5c24272addc68
Eugene Severin <zhokaogurca@gmail.com>
2024-01-10 08:10:16 +0700

    Added age evaluation

---
+++ evaluating_age.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int age;
+
+    cout << "Enter age" << endl;
+    cin >> age;
+
+    if(age<50)
+    {
+        cout << "Young" << endl;
+    }
+    
+    else
+    {
+        cout << "Not Young" << endl;
+    }
+
+}
