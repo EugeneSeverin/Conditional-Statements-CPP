92859662dffdaaabe204dff4a3ead00a516054b0
Eugene Severin <zhokaogurca@gmail.com>
2024-01-10 07:59:32 +0700

    Added theory on logical operators

---
+++ logical_operators.cpp
@@ -0,0 +1,37 @@
+// Logical Operators
+
+// && - AND
+
+// || - OR
+
+// ! - NOT
+
+// Truth Table
+
+// 1(True) and 1(True) = 1(True)
+
+// 1(True) and 0(False) = 0(False)
+
+// 0(False) and 1(True) = 0(False)
+
+// 0(False) and 0(False) = 0(False)
+
+// Truth(OR) Table
+
+// 1(True) OR 1(True) = 1(True)
+
+// 1(True) OR 0(False) = 1(False)
+
+// 0(False) OR 1(True) = 1(False)
+
+// 0(False) OR 0(False) = 0(False)
+
+// False Table
+
+// 1(True) NOT 1(True) = 0(False)
+
+// 1(True) NOT 0(False) = 1(True)
+
+// 0(False) NOT 1(True) = 1(True)
+
+// 0(False) NOT 0(False) = 0(False)
