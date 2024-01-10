607ffd333450c53360ec4082a708950b88cecdee
Eugene Severin <zhokaogurca@gmail.com>
2024-01-10 07:17:08 +0700

    added theory on if

---
+++ theory_on_if.cpp
@@ -0,0 +1,17 @@
+// Theory on if (Conditional Statement if)
+
+// If devides the process in between True/False
+
+// False = 0, True = 1 (or any other number: 2,3,4,5,6,7)
+
+// Basically the program only checks if a statement is 0(False) and in it's not, it's True
+
+
+// Conditions are written using conditional operators
+
+// Relational operators:
+
+// < , <= , > , >= , ==, != 
+
+// Also we use logical operators (it will be in the further files on theory)
+
