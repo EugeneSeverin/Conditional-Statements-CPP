168a7c5334a6565b9cddd2ab0126f55b7b9dea3b
Eugene Severin <zhokaogurca@gmail.com>
2024-01-10 08:06:26 +0700

    Added compund_conditional_statements

---
+++ compund_conditional_statements.cpp
@@ -0,0 +1,26 @@
+//  Hours - 23
+
+// Working hours 9 - 18
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int hour;
+
+    cout << "Enter an hour" << endl;
+    cin >> hour;
+
+    if(hour>=9 && hour<=18)
+    {
+        cout << "Working" << endl;
+    }
+    
+    else
+    {
+        cout << "Leisure" << endl;
+    }
+
+}
